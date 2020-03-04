d42a5cf6d2164816941d0c8d258efd21843509e3
Bernardo Santos <bernardosantos670@gmail.com>
2020-03-04 17:05:29 +0000

    Added notification template

--- html/templates/common.php
+++ html/templates/common.php
@@ -1,37 +1,36 @@
 <?php function doc_start($title, $stylesheets, $scripts) { ?>
-<!doctype html>
-<html lang="en">
+    <!doctype html>
+    <html lang="en">
 
-<head>
-    <title><?=$title?></title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <head>
+        <title><?=$title?></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <!-- JQuery, Popper and BootstrapJS scripts -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    
-    <!-- FontAwesome -->
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <!-- JQuery, Popper and BootstrapJS scripts -->
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-    <?php foreach($stylesheets as $stylesheet) { ?>
-        <link rel="stylesheet" href="../css/<?=$stylesheet?>.css">
-    <?php } ?>
+        <!-- FontAwesome -->
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
-    <?php foreach($scripts as $script) { ?>
-        <script src="../js/<?=$script?>.js" defer></script>
-    <?php } ?>
-</head>
-<body>
+        <?php foreach($stylesheets as $stylesheet) { ?>
+            <link rel="stylesheet" href="../css/<?=$stylesheet?>.css">
+        <?php } ?>
+
+        <?php foreach($scripts as $script) { ?>
+            <script src="../js/<?=$script?>.js" defer></script>
+        <?php } ?>
+    </head>
+    <body>
 <?php } ?>
 
 <?php function doc_end() { ?>
-</body>
-</html>
+    </body>
+    </html>
 <?php } ?>
-
 
 <?php function draw_header() { ?>
     <header>
@@ -44,10 +43,11 @@
                 <div class="dropdown d-flex align-items-center">
                     <span class="badge badge-pill badge-light mr-2">3</span>
                     <i class="fas fa-bell" data-toggle="dropdown"></i>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
+                    <div class="dropdown-menu dropdown-menu-right notification-menu">
+                        <?php draw_notification('çoasdhnfoçansdon oadjfoça çlkjadlçfjoa ojkj ajdsfoj alkçdfajshkniuer vbsoirhoibobcinsecoin'); ?>                     
+                        <?php draw_notification('çoasdhnfoçansdon oadjfoça çlkjadlçfjoa ojkj ajdsfoj alkçdfajshkniuer vbsoirhoibobcinsecoin'); ?>                     
+                        <?php draw_notification('çoasdhnfoçansdon oadjfoça çlkjadlçfjoa ojkj ajdsfoj alkçdfajshkniuer vbsoirhoibobcinsecoin'); ?>                     
+                        <?php draw_notification('çoasdhnfoçansdon oadjfoça çlkjadlçfjoa ojkj ajdsfoj alkçdfajshkniuer vbsoirhoibobcinsecoin'); ?>               
                     </div>
                 </div>
                 <div class="dropdown" style="margin-left: 1em">
@@ -87,4 +87,8 @@
             <li>Regulations</li>
         </ul>
     </footer>
+<?php } ?>
+
+<?php function draw_notification($text) { ?>
+    <a class="dropdown-item notification" href=""><?=$text?></a>
 <?php } ?>
