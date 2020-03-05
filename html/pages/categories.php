57b5e9225d1edeb7dad20a4646643cf6505df79d
CajoAlbuquerque <carlosjo.da@gmail.com>
2020-03-05 17:33:26 +0000

    Categories page comm news pin and hover effects

--- html/pages/categories.php
+++ html/pages/categories.php
@@ -21,42 +21,47 @@ include_once(__DIR__ . '/../templates/category.php');
         <button><i class="fas fa-plus"></i><strong> New Category</strong></button>
     </aside>
 
-    <!-- <main class="d-flex flex-wrap" id="feed"> -->
-    <main class="card-deck row row-cols-1 row-cols-lg-2 row-cols-xl-3" id="feed">
-        <div class="col mb-4">
-            <article class="card category-card" id="comm-news">
-                <div class="card-body">
-                    <header class="d-flex flex-row justify-content-between">
-                        <h5 class="card-title">! Community News</h5>
-                        <aside>
-                            <i class="fas fa-pen"></i>
-                            <i class="far fa-star"></i>
-                        </aside>
-                    </header>
-                </div>
-                <footer class="d-flex flex-row justify-content-between card-footer">
-                    <p><?= $posts ?> posts</p>
-                    <p>Last active: <?= $activity ?></p>
-                </footer>
-            </article>
-        </div>
-        <?php draw_category_card('Politics', '125k', '5 min') ?>
-        <?php draw_category_card('Gaming', '243k', '30 min') ?>
-        <?php draw_category_card('College', '326', '1 day') ?>
-        <?php draw_category_card('Bizarre', '1.3M', '2 days') ?>
-        <?php draw_category_card('Drunk', '521k', '3 days') ?>
-        <?php draw_category_card('Economy', '331k', '12 days') ?>
-        <?php draw_category_card('Work', '989', '20 day') ?>
-        <?php draw_category_card('Teenager', '473', '2 months') ?>
-        <?php draw_category_card('Countries', '10k', '6 months') ?>
-        <?php draw_category_card(' Story', '1k', '10 months') ?>
-        <?php draw_category_card('Sad Story', '1k', '10 months') ?>
-        <?php draw_category_card('Sad Story', '1k', '10 months') ?>
-        <?php draw_category_card('Sad Story', '1k', '10 months') ?>
-        <?php draw_category_card('Sad Story', '1k', '10 months') ?>
-        <?php draw_category_card('Other', '326', '1 year') ?>
+    <main id="feed">
+        <section class="card-deck row row-cols-1 row-cols-lg-2 row-cols-xl-3" id="comm-news">
+            <div class="col mb-4 highlight"><i class="fas fa-grip-lines"></i></div>
+            <div class="col mb-4">
+                <article class="card category-card">
+                    <div class="card-body">
+                        <header class="d-flex flex-row justify-content-between">
+                            <h5 class="card-title">! Community News</h5>
+                            <aside>
+                                <i class="fas fa-pen"></i>
+                                <i class="far fa-star"></i>
+                            </aside>
+                        </header>
+                    </div>
+                    <footer class="d-flex flex-row justify-content-between card-footer">
+                        <p>23 posts</p>
+                        <p>Last active: 15 days</p>
+                    </footer>
+                </article>
+            </div>
+            <div class="col mb-4 highlight"><i class="fas fa-grip-lines"></i></div>
+        </section>
+
+        <section class="card-deck row row-cols-1 row-cols-lg-2 row-cols-xl-3">
+            <?php draw_category_card('Politics', '125k', '5 min') ?>
+            <?php draw_category_card('Gaming', '243k', '30 min') ?>
+            <?php draw_category_card('College', '326', '1 day') ?>
+            <?php draw_category_card('Bizarre', '1.3M', '2 days') ?>
+            <?php draw_category_card('Drunk', '521k', '3 days') ?>
+            <?php draw_category_card('Economy', '331k', '12 days') ?>
+            <?php draw_category_card('Work', '989', '20 day') ?>
+            <?php draw_category_card('Teenager', '473', '2 months') ?>
+            <?php draw_category_card('Countries', '10k', '6 months') ?>
+            <?php draw_category_card(' Story', '1k', '10 months') ?>
+            <?php draw_category_card('Sad Story', '1k', '10 months') ?>
+            <?php draw_category_card('Sad Story', '1k', '10 months') ?>
+            <?php draw_category_card('Sad Story', '1k', '10 months') ?>
+            <?php draw_category_card('Sad Story', '1k', '10 months') ?>
+            <?php draw_category_card('Other', '326', '1 year') ?>
+        </section>
     </main>
-    <!-- </main> -->
 </div>
 
 <?php draw_footer(); ?>
