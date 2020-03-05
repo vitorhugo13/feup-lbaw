edece1f98a9228d0ae1802d9a4f88e10802c6140
Bernardo Santos <bernardosantos670@gmail.com>
2020-03-05 18:03:22 +0000

    Started working on reports page

---
+++ html/pages/reports.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once(__DIR__.'/../templates/common.php');
+include_once(__DIR__.'/../templates/post_elems.php');
+
+?>
+
+<?php doc_start('Reports', ['common', 'post_elems', 'reports'], ['top']); ?>
+<?php draw_header(); ?>
+
+<aside class="sidebar">
+    
+</aside>
+
+<?php draw_move_modal(); ?>
+<?php draw_back_to_top(); ?>
+
+<?php draw_footer(); ?>
+<?php doc_end(); ?>
