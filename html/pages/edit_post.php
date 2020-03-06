6b7b83490148aa8376cb15bdc8dec4c9a377cf39
Bernardo Santos <bernardosantos670@gmail.com>
2020-03-06 22:34:03 +0000

    Started work on edit post

---
+++ html/pages/edit_post.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once(__DIR__.'/../templates/common.php');
+include_once(__DIR__.'/../templates/post_elems.php');
+include_once(__DIR__.'/../templates/report_elems.php');
+
+?>
+
+<?php doc_start('Edit Post', ['common', 'post_elems', 'edit_post'], ['top']); ?>
+<?php draw_header(); ?>
+
+
+
+
+
+
+<?php draw_back_to_top(); ?>
+
+<?php draw_footer(); ?>
+<?php doc_end(); ?>
