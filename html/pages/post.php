14b4f918b8ec98341390ccddad54f71f9831693d
Tito Griné <tito999@gmail.com>
2020-03-07 01:34:57 +0000

    Progress on post page.

--- html/pages/post.php
+++ html/pages/post.php
@@ -11,15 +11,15 @@ include_once(__DIR__.'/../templates/post_elems.php');
 <div class="post">
     <header class="d-flex flex-column">
         <div class="d-flex flex-row align-items-center justify-content-between">
-            <div>    
-                <a href="#"><img class="rounded-circle" src="../assets/user.jpg" width="50"></a>
+            <div class="post-user d-flex flex-row align-items-center justify-content-between">    
+                <a href="#"><img class="rounded-circle" src="../assets/user.jpg" width="40"></a>
                 <a href="#">lamao</a>
                 <span>&middot;</span>
                 <span>17h ago</span>
             </div>
-            <div class="d-flex flex-row">
+            <div class="d-flex flex-row align-items-center">
                 <i class="far fa-star mr-3"></i>
-                <div class="dropdown">
+                <div class="dropdown d-flex align-items-center">
                     <i class="fas fa-ellipsis-v" data-toggle="dropdown"></i>
                     <div class="dropdown-menu dropdown-menu-right">
                         <a class="dropdown-item" data-toggle="modal" data-target="#report-modal">Report</a>
@@ -33,7 +33,7 @@ include_once(__DIR__.'/../templates/post_elems.php');
                 </div>
             </div>
         </div>
-        <h1>Title title</h1>
+        <h4> I had the greatest job interview in the world, 5000 people applied and I won. I now travel the world and talk about whisky for a living. AMA</h1>
         <div class="post-categories">
             <?php draw_category_badge('Politics'); ?>
             <?php draw_category_badge('Gaming'); ?>
@@ -41,12 +41,14 @@ include_once(__DIR__.'/../templates/post_elems.php');
         </div>
     </header>
     <div class="post-body">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam aliquam in tellus sit amet euismod. Ut at mi id nunc ultrices tincidunt sit amet in nisl. Nunc consectetur diam sed purus commodo, eu malesuada nisl dapibus. Vestibulum id urna tincidunt, accumsan felis sit amet, aliquam metus. Vivamus auctor metus quis felis rhoncus, eget hendrerit massa vulputate. In hac habitasse platea dictumst. Praesent sed purus interdum, semper nibh in, feugiat odio. Donec quis lobortis quam, a aliquet ante. Etiam est nisi, accumsan non mi sit amet, viverra ornare dui. Suspendisse tincidunt venenatis nulla, non aliquet augue. Sed lobortis a quam at faucibus. Morbi euismod ipsum eget condimentum convallis. Quisque ultrices varius magna, sed congue eros rutrum eu. Praesent mauris eros, placerat sit amet venenatis et, vehicula sit amet augue.</p>
-        <p>Nunc commodo purus malesuada auctor efficitur. Aenean libero risus, semper sed fermentum at, suscipit eu ligula. Cras bibendum est at lorem congue, vel dignissim diam convallis. Mauris id nunc fringilla, mollis mauris in, congue orci. Maecenas condimentum fringilla orci, eget tempor velit ultricies non. Sed lacinia lacus sit amet dui elementum, eu vehicula tellus fermentum. Cras eget vehicula tellus. Morbi leo elit, mollis et suscipit id, malesuada non neque. Ut gravida mauris leo, nec dictum libero consectetur nec. Etiam ante diam, posuere in elementum tempor, hendrerit at leo. Aenean porttitor porttitor ex, consectetur sollicitudin ligula elementum eget. Vestibulum sit amet nunc sed justo pharetra tristique. Pellentesque magna lacus, sagittis ut pellentesque non, venenatis nec eros.</p>
+        <p>Last year I applied for a job to become a Global Whisky Ambassador for Grant’s Scotch Whisky. They advertised the role across multiple social media websites so they had a lot of attention from all over the world.</p>
+        <p>5000 people applied and I won.</p>
+        <p>Past year I have been travelling the world talking about whisky and I have just been nominated as Scotch Whisky Ambassador Of The Year by Icons of Whisky.</p>
+        <p>If you want to see some pictures and videos of my journey so far check out my IG @dannydyer</p>
     </div>
     <footer class="d-flex flex-row align-items-center">
-        <div class="upvotes"><i class="far fa-thumbs-up"></i>2k</div>
-        <div class="downvotes"><i class="far fa-thumbs-down"></i>407</div>
+    <div class="upvotes"><img src="../assets/hoof_filled.svg" width="13" alt="uphoof"/> 2062</div>
+    <div class="downvotes"><img src="../assets/hoof_outline.svg" width="13" alt="downhoof"/>  407</div>
     </footer>
 </div>
 <div id="comment-section">
