502da5c587dc230e195b0f3f745da4651bce5c58
Bernardo Santos <bernardosantos670@gmail.com>
2020-03-07 14:17:49 +0000

    Edit post styling

--- html/pages/edit_post.php
+++ html/pages/edit_post.php
@@ -9,10 +9,40 @@ include_once(__DIR__.'/../templates/report_elems.php');
 <?php doc_start('Edit Post', ['common', 'post_elems', 'edit_post'], ['top']); ?>
 <?php draw_header(); ?>
 
+<form>
+    <div id="sidebar">
+        <div>
+            <header>Categories</header>
+            <div class="input-group">
+                <select class="custom-select">
+                    <option selected>Add new category...</option>
+                    <option value="1">College</option>
+                    <option value="2">World</option>
+                    <option value="3">Economics</option>
+                    <option value="3">Cinema</option>
+                    <option value="3">Music</option>
+                </select>
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button">Add</button>
+                </div>
+            </div>
+            <div class="selected-categories">
+                <?php draw_category_move_badge('Politics'); ?>
+                <?php draw_category_move_badge('Gaming'); ?>
+                <?php draw_category_move_badge('Ethics'); ?>
+            </div>
+        </div>
+        <div class="d-flex flex-row justify-content-center">
+            <button class="btn btn-secondary">Cancel</button>
+            <button class="btn btn-primary" type="submit">Post</button>
+        </div>    
+    </div>
 
-
-
-
+    <div id="main" class="d-flex flex-column justify-content-stretch">
+        <input type="text" id="post-title" placeholder="Title"/>
+        <textarea id="post-body" placeholder="What is this post about?" rows="3"></textarea>
+    </div>
+</form>
 
 <?php draw_back_to_top(); ?>
 
