a1dea49dcb5917b14505a89af6965f593c18579c
Tito Griné <tito999@gmail.com>
2020-03-07 18:06:09 +0000

    Finished profile page styling.

--- html/pages/profile.php
+++ html/pages/profile.php
@@ -10,10 +10,10 @@ include_once(__DIR__.'/../templates/post_elems.php');
 
 <div class="profile-container">
     <div class="row">
-        <div class="col-test col-12 col-lg-3 d-flex flex-column align-items-center"> 
+        <div class="info-card col-test col-12 col-lg-3 d-flex flex-column align-items-center"> 
             <img src="../assets/team_photos/vitorhugo.jpg" class="img rounded-circle  mt-2" alt="Profile photo">
             <div class="username d-flex flex-row align-items-center">
-                <h3>vitorhugo_5</h3>
+                <p>vitorhugo_5</p>
                 <div class="dropdown col-1">        
                 <div  data-toggle="dropdown"><i class="fas fa-ellipsis-v"></i></div>
                     <div class="dropdown-menu dropdown-menu-right">
@@ -27,49 +27,48 @@ include_once(__DIR__.'/../templates/post_elems.php');
                 </div>
             </div>
             <div class="d-flex flex-wrap flex-column">
-                <div class=" bio">
-                    <i class="fab fa-font-awesome-flag"></i> Adoro desporto e ecónomia. Sou uma pessoa bastante respeitadora, portanto também exigo que o sejam comigo. Abraço! 
-                </div>
+                <p class="bio text-left">
+                    Adoro desporto e economia. Sou uma pessoa bastante respeitadora, portanto também exigo que o sejam comigo. Abraço! 
+                </p>
             </div>
 
-            <div>
-                <a class="btn-2" href="./edit_profile.php"><i class="fas fa-pencil-alt"></i><strong>  Edit profile</strong></a>
-
+            <div class="d-flex flex-column align-items-center">
+                <a class="edit-button" href="./edit_profile.php"></i><strong>Edit profile</strong></a>
             </div>
         </div> 
 
-        <div class="col-12 col-md-5 col-lg-4 glory"> 
-            <img src="../assets/logo_text.svg"  width="60" alt="photo">
-            <div class="points"> <h2>12903 points</h2></div>
-            <div class="contest">
-                <div class="contest-text">
-                    <h4>You are blocked for</h4>
+        <div class="side-info col-12 col-md-5 col-lg-4"> 
+            <img src="../assets/gold_llama.svg"  width="80" alt="photo">
+            <div class="points"> <p>&diams; 12903 points &diams;</p></div>
+            <div class="blocked">
+                <div class="blocked-text">
+                    <p>You are blocked for:</p>
                 </div>
                 <div class="remaining-time">
-                    <h2>49h 30m 06s</h2>
+                    <p>49h 30m 06s</p>
                 </div>
-                <button class="btn"><i class="fas fa-exclamation-circle"></i><strong>  Contest</strong></button>
+                <button class="contest-button"><i class="fas fa-exclamation-circle"></i><strong>  Contest</strong></button>
             </div>
         </div>
 
         <div class="col-12 col-md-7 col-lg-5 top_categories">
-            <div class="table_categories">
+            <div class="">
                 <div class="categories-text">
-                    <h2>Top Categories</h2>
+                    <h4>Top Categories</h4>
                 </div>
                 <div class="list-top">
                     <div class="top_category d-flex justify-content-between">
-                        <span><i class="fas fa-medal"></i> !Sports</span>
-                        <span>8k</span>
+                        <span><i class="fas fa-medal"></i> ! Sports</span>
+                        <span>8567</span>
                     </div>
 
                     <div class="top_category d-flex justify-content-between">
-                        <span><i class="fas fa-medal"></i> !Economy</span>
-                        <span>2k</span>
+                        <span><i class="fas fa-medal"></i> ! Economy</span>
+                        <span>2103</span>
                     </div>
 
                     <div class="top_category d-flex justify-content-between">
-                        <span><i class="fas fa-medal"></i> !Literature</span>
+                        <span><i class="fas fa-medal"></i> ! Literature</span>
                         <span>783</span>
                     </div>
                 </div>
@@ -79,45 +78,31 @@ include_once(__DIR__.'/../templates/post_elems.php');
 
 
 
-    <div class="row">
+    <div class="post-section row">
         <div class="col-12 col-lg-3">
         </div>
         <div class="col-12 col-lg-9"> 
 
-            <div class="number-posts"> Posts (19)</div>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-            <?php draw_post_preview('Title', 'username', 'This is the post content preview', '50k', '360', '1230'); ?>
-        </div>
+            <div class="number-posts align-baseline"> <strong>Posts </strong> (19)</div>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            <?php draw_post_preview('Title', 'vitorhugo_5', 'This is the post content preview', '50k', '360', '1230'); ?>
+            </div>
     </div>
 </div>
 
