92388fbdb66aaff96a2a9b459761dcb7a8b78c54
Cajo Albuquerque <carlosjo.da@gmail.com>
2020-03-08 01:35:42 +0000

    Reports page responsive table and overflow text

--- html/templates/report_elems.php
+++ html/templates/report_elems.php
@@ -1,8 +1,15 @@
-<?php function draw_report($title, $reason, $date) { ?>
+<?php function draw_report($title, $reason, $date)
+{ ?>
     <tr>
-        <td><?=$title?></td>
-        <td><?=$reason?></td>
-        <td><?=$date?></td>
+        <td class="data-title">
+            <span><?= $title ?></span>
+        </td>
+        <td class="data-reason">
+            <span><?= $reason ?></span>
+        </td>
+        <td class="data-date">
+            <span><?= $date ?></span>
+        </td>
         <td class="dropdown">
             <i class="fas fa-ellipsis-v" data-toggle="dropdown"></i>
             <div class="dropdown-menu dropdown-menu-right">
