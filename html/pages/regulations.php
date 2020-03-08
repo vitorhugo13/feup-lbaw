2d59fb1166b44324ef224fb537ba1b8178f3f7e5
Tito Griné <tito999@gmail.com>
2020-03-08 17:36:33 +0000

    Last mminute style changes.

--- html/pages/regulations.php
+++ html/pages/regulations.php
@@ -75,7 +75,7 @@ include_once(__DIR__.'/../templates/common.php');
         <p><span>15 &middot;</span> You can not use the Services in any manner that could interfere with, disrupt, negatively affect, or inhibit other users from fully enjoying the Services or that could damage, disable, overburden, or impair the functioning of the Services in any manner.</p>
     </div>
 
-    <img src="../assets/logo_image.png" class="img mt-5" alt="Lama photo">
+    <img src="../assets/happy_llama.svg" width="150px" class="img mt-5" alt="Lama photo">
     <h3 class="ty mt-1">Thank you!</h3>
 </div>
 
