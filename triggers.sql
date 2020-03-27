d222aabf630907ecd56a3d637dd781b5fce2ee24
Bernardo Santos <bernardosantos670@gmail.com>
2020-03-27 23:09:36 +0000

    Implemented all vote triggers

--- triggers.sql
+++ triggers.sql
@@ -1,8 +1,13 @@
 --------------------------------- drop existing triggers -------------------------------------
 
-DROP TRIGGER IF EXISTS new_vote ON rating;
+DROP TRIGGER IF EXISTS add_vote ON rating;
 DROP FUNCTION IF EXISTS add_vote();
 
+DROP TRIGGER IF EXISTS rem_vote ON rating;
+DROP FUNCTION IF EXISTS rem_vote();
+
+DROP TRIGGER IF EXISTS update_vote ON rating;
+DROP FUNCTION IF EXISTS update_vote();
 
 --------------------------------- triggers ---------------------------------
 
@@ -10,10 +15,8 @@ CREATE FUNCTION add_vote() RETURNS TRIGGER AS
 $BODY$
 BEGIN
     IF NEW.rating::text = 'upvote' THEN
-        -- increment upvotes on content
         UPDATE content SET upvotes = upvotes + 1 WHERE id = NEW.content;
     END IF;
-
     IF NEW.rating::text = 'downvote' THEN
         UPDATE content SET downvotes = downvotes + 1 WHERE id = NEW.content;
     END IF;
@@ -22,7 +25,54 @@ END
 $BODY$
 LANGUAGE plpgsql;
 
-CREATE TRIGGER new_vote 
+CREATE TRIGGER add_vote 
     AFTER INSERT ON rating
     FOR EACH ROW
     EXECUTE PROCEDURE add_vote();
+
+
+CREATE FUNCTION rem_vote() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    IF OLD.rating::text = 'upvote' THEN
+        UPDATE content SET upvotes = upvotes - 1 WHERE id = OLD.content;
+    END IF;
+    IF OLD.rating::text = 'downvote' THEN
+        UPDATE content SET downvotes = downvotes - 1 WHERE id = OLD.content;
+    END IF;
+    RETURN OLD;
+END
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER rem_vote 
+    AFTER DELETE ON rating
+    FOR EACH ROW
+    EXECUTE PROCEDURE rem_vote();
+
+
+CREATE FUNCTION update_vote() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    IF OLD.rating::text = 'upvote' THEN
+        UPDATE content SET upvotes = upvotes - 1 WHERE id = OLD.content;
+    END IF;
+    IF OLD.rating::text = 'downvote' THEN
+        UPDATE content SET downvotes = downvotes - 1 WHERE id = OLD.content;
+    END IF;
+
+    IF NEW.rating::text = 'upvote' THEN
+        UPDATE content SET upvotes = upvotes + 1 WHERE id = NEW.content;
+    END IF;
+    IF NEW.rating::text = 'downvote' THEN
+        UPDATE content SET downvotes = downvotes + 1 WHERE id = NEW.content;
+    END IF;
+    RETURN NEW;
+END
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER update_vote 
+    AFTER DELETE ON rating
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_vote();
