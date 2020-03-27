cb9aaec4ec62771770294cc40ababca17d4ff150
CajoAlbuquerque <carlosjo.da@gmail.com>
2020-03-27 23:03:11 +0000

    Tested trigger

--- triggers.sql
+++ triggers.sql
@@ -1,20 +1,21 @@
 --------------------------------- drop existing triggers -------------------------------------
 
 DROP TRIGGER IF EXISTS new_vote ON rating;
+DROP FUNCTION IF EXISTS add_vote();
 
 
 --------------------------------- triggers ---------------------------------
 
-CREATE FUNCTION add_vote(IN content_id INTEGER, IN type TEXT) RETURNS TRIGGER AS
+CREATE FUNCTION add_vote() RETURNS TRIGGER AS
 $BODY$
 BEGIN
-    IF type = 'upvote' THEN
+    IF NEW.rating::text = 'upvote' THEN
         -- increment upvotes on content
-        UPDATE content SET upvotes = upvotes + 1 WHERE id = content_id;
+        UPDATE content SET upvotes = upvotes + 1 WHERE id = NEW.content;
     END IF;
 
-    IF type = 'downvote' THEN
-        UPDATE content SET downvotes = downvotes + 1 WHERE id = content_id;
+    IF NEW.rating::text = 'downvote' THEN
+        UPDATE content SET downvotes = downvotes + 1 WHERE id = NEW.content;
     END IF;
     RETURN NEW;
 END
@@ -24,4 +25,4 @@ LANGUAGE plpgsql;
 CREATE TRIGGER new_vote 
     AFTER INSERT ON rating
     FOR EACH ROW
-    EXECUTE PROCEDURE add_vote(NEW.content, NEW.rating::text);
+    EXECUTE PROCEDURE add_vote();
