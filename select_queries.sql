a7ccd5962e7f7d5e3342111fd34c0d652646d2a5
Tito Griné <tito999@gmail.com>
2020-03-28 22:13:35 +0000

    Added more queries.

--- select_queries.sql
+++
@@ -1,4 +0,0 @@
------------ Selecting info for post page ---------
-SELECT author, title, body, visible, tracking, creation_time, upvotes, downvotes, num_comments
-FROM content JOIN post ON id
-WHERE post.id=$id;
