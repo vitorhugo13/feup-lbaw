13cdfa5051adc4652b3420d4660c426fbea75501
Tito Griné <tito999@gmail.com>
2020-03-29 17:06:19 +0100

    Minor change in indexes.

--- indexes.sql
+++ indexes.sql
@@ -1,12 +1,12 @@
-CREATE INDEX user_username ON "user" USING hash(username);
+CREATE INDEX user_username ON "user" USING hash (username);
 
-CREATE INDEX rating_time ON "rating" USING btree("time");
+CREATE INDEX rating_time ON "rating" USING btree ("time");
 
-CREATE INDEX report_time ON "report" USING btree("time");
+CREATE INDEX report_time ON "report" USING btree ("time");
 
-CREATE INDEX content_upload ON "content" USING btree("creation_time");
+CREATE INDEX report_time ON "report" USING btree ("time");
 
-CREATE INDEX glory_category ON "category_glory" USING btree("glory");
+CREATE INDEX glory_category ON "category_glory" USING btree ("glory");
 
 CREATE INDEX title_search ON post USING gist ('english', title);
 
