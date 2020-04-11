fe115fab8a72fc1e710791e2fdbabb2a389b2639
CajoAlbuquerque <carlosjo.da@gmail.com>
2020-04-11 16:36:13 +0100

    moved everything to a folder

--- template_laravel/database/seeds/DatabaseSeeder.php
+++ template_laravel/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
          Eloquent::unguard();
 
          $path = 'resources/sql/seed.sql';
+         $this->command->info($path);
          DB::unprepared(file_get_contents($path));
          $this->command->info('Database seeded!');
      }
