103dcd023cafe1acd81f36375bf194dd54d7c3ba
Bernardo Santos <bernardosantos670@gmail.com>
2020-04-15 22:54:44 +0100

    Added 403 view

---
+++ resources/views/pages/403.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+{{-- TODO: the layout to be used must have the header and footer --}}
+@section('content')
+
+@push('styles')
+    <link href="{{ asset('css/common.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/error_pages.css') }}" rel="stylesheet">
+@endpush
+
+
+{{-- TODO: change the redirect to the homepage --}}
+<div class="error text-center">
+    <img src="{{ asset('images/suspicious_llama.svg') }}" width="170px" class="lama-img mt-2" alt="Lama photo">
+    <h3 class="error-404">403 Access denied!</h3>
+    <p class="error-404-p">You don't have permission to open this page...</p>
+    <a href="{{ route('home') }}"><button type="submit" class="btn"><i class="fas fa-undo-alt"></i>   Return</button></a>
+</div>
