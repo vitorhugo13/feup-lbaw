121eddda792ad95d6bb3eafb222db526d15fa9cf
Bernardo Santos <bernardosantos670@gmail.com>
2020-04-17 18:31:02 +0100

    navbar updated

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,37 +11,7 @@
 
 @section('content')
 
-<header>
-    <nav class="navbar">
-    <a class="navbar-brand" href="{{url('home')}}"><img src="{{asset('images/logo_text.svg')}}" width="50" alt="LAMA logo" /></a>
-        <form class="expandable-search" action="{{url('search')}}">
-            <input type="search" placeholder="Search" aria-label="Search" />
-        </form>
-        <div class="d-flex align-items-center">
-            <div class="dropdown d-flex align-items-center">
-                <span class="badge badge-pill badge-light mr-2">3</span>
-                <i class="fas fa-bell" data-toggle="dropdown"></i>
-                <div class="dropdown-menu dropdown-menu-right notification-menu">
-                    
-                </div>
-            </div>
-            <div class="dropdown" style="margin-left: 1em">
-                <img class="rounded-circle dropdown-toggle" data-toggle="dropdown" src="{{ asset('images/default_picture.png') }}" height="30">
-                <div class="dropdown-menu dropdown-menu-right">
-                    <a class="dropdown-item" href="">Profile</a>
-                    <a class="dropdown-item" href="">Feed</a>
-                    <a class="dropdown-item" href="">Reports</a>
-                    <a class="dropdown-item" href=""><i class="fas fa-ban"></i> 49:30:06</a>
-                    <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="{{url('logout')}}">Log Out</a>
-                </div>
-            </div>
-        </div>
-    </nav>
-</header>
-<form action="{{url('search')}}" id="collapsed-search" class="d-flex flex-row justify-content-center">
-    <input type="search" placeholder="Search" aria-label="Search" />
-</form>
+@include('partials.common.navbar')
 
 <div id="content">
     @yield('main-content')
