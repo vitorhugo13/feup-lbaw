4aa8d356d3b822cb0ff60961b88fda43723c509c
Bernardo Santos <bernardosantos670@gmail.com>
2020-04-18 22:47:36 +0100

    Created a partial for ratings

---
+++ resources/views/partials/content/rating.blade.php
@@ -0,0 +1,13 @@
+@auth
+@php 
+    $rating = App\Models\Rating::where('user_id', Auth::user()->id)->where('content', $content->id)->first();
+    $rating = ($rating == null) ? '' : $rating->rating;
+@endphp
+@endauth
+@guest
+@php
+    $rating = '';
+@endphp
+@endguest
+<div class="upvotes mr-3 @if($rating == 'upvote') selected @endif" data-id="{{ $content->id }}"><img src="{{ asset('images/hoof_filled.svg') }}" width="13" alt="uphoof" /> +<span>{{ $content->upvotes }}</span></div>
+<div class="downvotes mr-3 @if($rating == 'downvote') selected @endif" data-id="{{ $content->id }}"><img src="{{ asset('images/hoof_outline.svg') }}" width="13" alt="downhoof" /> -<span>{{ $content->downvotes }}</span></div>
