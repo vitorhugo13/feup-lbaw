a65f72779c735adfd74f2b9d68e8207e9a2093c2
Bernardo Santos <bernardosantos670@gmail.com>
2020-04-19 19:09:57 +0100

    Fixed bug in post deletion

--- resources/views/partials/posts/options.blade.php
+++ resources/views/partials/posts/options.blade.php
@@ -33,7 +33,12 @@
                 @if ($author != null && Auth::user()->id == $author->id)
                     <a class="dropdown-item" href="{{ route('edit', $post->id) }}">Edit</a>
                     <a class="dropdown-item" href="#">Mute</a>
-                    <a class="dropdown-item" href="#">Delete</a>
+                    <a class="dropdown-item">
+                        <form method="POST" action="{{ route('delete', $post->id) }}" onclick='submit()'>
+                        @csrf @method('DELETE')
+                        Delete
+                        </form>
+                    </a>
                 @else
                     <a class="dropdown-item">
                         <form method="POST" action="{{ route('delete', $post->id) }}" onclick='submit()'>
