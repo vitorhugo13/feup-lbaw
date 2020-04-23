899a8a8dbc7b05c4cfd41f824ed7a86cb316da68
Bernardo Santos <bernardosantos670@gmail.com>
2020-04-23 22:24:28 +0100

    Fixed conflicts

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Validator;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\File;
-=======
 use Illuminate\Foundation\Auth\RegistersUsers;
->>>>>>> bb65c144164991c5310be44828f600522255eecb
 
 use App\Models\User;
 
