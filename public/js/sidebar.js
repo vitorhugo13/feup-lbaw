07bbbb2b212bbf93c20457ed2b664c60e4c2bff1
CajoAlbuquerque <carlosjo.da@gmail.com>
2020-05-14 22:33:09 +0100

    Sidebar not overlaping footer

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -5,9 +5,14 @@ let sidebar = document.getElementById('sidebar')
 let btn = document.getElementById('side-toggle')
 let angleLeft = document.getElementById('angle-left')
 let angleRight = document.getElementById('angle-right')
+let footer = document.getElementById('footer')
+
+let sidebarTopOffset = sidebar.offsetTop
+let topAcc = 0
+let lastScroll = 0
 
 function toggleSidebar() {
-    if(sidebar.classList.contains('active')){
+    if (sidebar.classList.contains('active')) {
         collapse()
     } else {
         show()
@@ -29,5 +34,29 @@ function show() {
     btn.style['background-color'] = 'var(--main-color)'
 }
 
+function relocateSidebar() {
+    let windowTop = window.pageYOffset
+    let footerTop = footer.offsetTop
+    let sidebarTop = sidebar.offsetTop
+    let sidebarHeight = sidebar.offsetHeight
+
+    let margin = 10
+
+    if ((windowTop + sidebarTop + sidebarHeight) > (footerTop - margin)) {
+        let offset = footerTop - margin - windowTop - sidebarHeight
+
+        sidebar.style.top = offset + 'px'
+        topAcc = offset
+    } else if (footerTop - windowTop > sidebarHeight + sidebarTopOffset) {
+        sidebar.style.top = 'auto'
+    } else if (footerTop - windowTop > sidebarHeight) {
+        topAcc -= windowTop - lastScroll
+        sidebar.style.top = topAcc + 'px'
+    }
+
+    lastScroll = windowTop
+}
+
 overlay.addEventListener('click', toggleSidebar)
 btn.addEventListener('click', toggleSidebar)
+window.addEventListener('scroll', relocateSidebar)
