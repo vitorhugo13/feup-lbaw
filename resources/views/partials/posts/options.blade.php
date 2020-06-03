59ebf380d2050cd27b7835d488594fc8d7e749e2
Bernardo Santos <bernardosantos670@gmail.com>
2020-06-03 14:22:33 +0100

    Removed some wrong options in post dropdown

--- resources/views/partials/posts/options.blade.php
+++ resources/views/partials/posts/options.blade.php
@@ -38,14 +38,7 @@
                         </form>
                     </a>
                 @else
-                    <a class="dropdown-item">
-                        <form method="POST" action="{{ route('delete', $post->id) }}" onclick='submit()'>
-                        @csrf @method('DELETE')
-                        Delete
-                        </form>
-                    </a>
                     <a class="dropdown-item" data-toggle="modal" data-target="#report-modal"  data-content-id="{{$post->id}}">Report</a>
-                    <a class="dropdown-item" href="#">Resolve</a>
                     <a class="dropdown-item" data-toggle="modal" data-target="#move-modal">Move</a>
                 @endif
         @endswitch
