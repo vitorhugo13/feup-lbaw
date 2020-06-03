209b644980d0b9108021bd7dae643cfafdc14993
Bernardo Santos <bernardosantos670@gmail.com>
2020-06-03 14:24:35 +0100

    Removed unnecessary options from comment dropdown

--- resources/views/partials/comment/options.blade.php
+++ resources/views/partials/comment/options.blade.php
@@ -5,15 +5,11 @@
             @case('Blocked')
                 @if ($author != null && Auth::user()->id == $author->id)
                     <a class="dropdown-item delete-comment-btn" data-toggle="modal" data-target="#delete-modal" data-comment-id="{{ $comment->id }}">Delete</a>
-                    <a class="dropdown-item" href="#">Mute</a>
-                @else
-                    <a class="dropdown-item" href="#">Block User</a>
                 @endif
                 @break
             @case('Member')
                 @if ($author != null && Auth::user()->id == $author->id)
                     <a class="dropdown-item edit-comment-btn" data-thread-id="{{ $thread_id }}" data-comment-id="{{ $comment->id }}">Edit</a>
-                    <a class="dropdown-item" href="#">Mute</a>
                     <a class="dropdown-item delete-comment-btn" data-toggle="modal" data-target="#delete-modal" data-comment-id="{{ $comment->id }}">Delete</a>
                 @else
                     <a class="dropdown-item" data-toggle="modal" data-target="#report-modal"  data-content-id="{{$comment->id}}">Report</a>
@@ -22,14 +18,9 @@
             @default
                 @if ($author != null && Auth::user()->id == $author->id)
                     <a class="dropdown-item edit-comment-btn" data-thread-id="{{ $thread_id }}" data-comment-id="{{ $comment->id }}">Edit</a>
-                    <a class="dropdown-item" href="#">Mute</a>
                     <a class="dropdown-item delete-comment-btn" data-toggle="modal" data-target="#delete-modal" data-comment-id="{{ $comment->id }}">Delete</a>
                 @else
-                    <a class="dropdown-item delete-comment-btn" data-toggle="modal" data-target="#delete-modal" data-comment-id="{{ $comment->id }}">Delete</a>
                     <a class="dropdown-item" data-toggle="modal" data-target="#report-modal" data-content-id="{{$comment->id}}">Report</a>
-                    <a class="dropdown-item" href="#">Resolve</a>
-                    <a class="dropdown-item" data-toggle="modal" data-target="#move-modal">Move</a>
-                    <a class="dropdown-item" href="#">Block User</a>
                 @endif
         @endswitch
     </div>
